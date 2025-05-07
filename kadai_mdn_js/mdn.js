cffc33e03fb7a3fde3adbeda7ed4fb99602eaf3a
wakobe <be.wako2025@gmail.com>
2025-05-07 20:47:13 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,13 @@
+const today = new Date();
+const year = today.getFullYear();
+const month= today.getMonth()+1;
+const date = today.getDate();
+
+console.log(year + '年' + month + '月' + date + '日');
+
+// メモ
+// const today = new Date();
+
+// console.log(today.getFullYear() + '年' +
+//  (today.getMonth()+1) + '月' + today.getDate() + '日');
+// sbq-kfgd-psa
